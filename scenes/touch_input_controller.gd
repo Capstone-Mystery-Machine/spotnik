2ae957333631a67f0a10a66a889e850ff52d7488
oridim <125223080+oridim@users.noreply.github.com>
2026-01-13 09:30:38 -0500

    docs: initial non-impl docstrings for `TouchInputController`

--- scenes/touch_input_controller.gd
+++ scenes/touch_input_controller.gd
@@ -1,17 +1,33 @@
 extends Node
+## Input controller that targets a [Node3D].
+##
+## The input controller modifies a target [Node3D]'s [member Node3D.transform]
+## in response to the end-user tapping-and-dragging on the 3D viewport.
 
 @export_group("Targeting")
 
+## Represents which [Node3D] that the input controller is going to modify in
+## reaction to input performed by the end-user.
 @export var target_node_3d: Node3D = null
 
 @export_group("Control Settings")
 
+## Represents the sensitivity value that is [b]multiplied against[/b] the
+## end-user's vertical touch movements. That is, the lower the value, the more
+## physical dragging movement needed to modify the target [Node3D]'s
+## [member Node3D.transform].
 @export var sensitity_pitch: float = 0.0025
 
+## Represents the sensitivity value that is [b]multiplied against[/b] the
+## end-user's horizontal touch movements. That is, the lower the value, the more
+## physical dragging movement needed to modify the target [Node3D]'s
+## [member Node3D.transform].
 @export var sensitity_yaw: float = 0.0025
 
 var is_dragging: bool = false
 
+## Enables the input controller if [constant InputX.input_mode] is set to
+## [constant InputX.InputMode.INPUT_TOUCH].
 func _ready() -> void:
 	match InputX.input_mode:
 		InputX.InputMode.INPUT_TOUCH:
