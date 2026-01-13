1f5f4f4b1c2e6c7aeeddb7a3d6301da3209b8992
oridim <125223080+oridim@users.noreply.github.com>
2026-01-13 15:33:11 -0500

    feat: added utility singleton containing single / double precision epsilons

---
+++ scripts/global_scope_x.gd
@@ -0,0 +1,11 @@
+class_name GlobalScopeX
+extends Node
+## A singleton for providing general-purpose utility API.
+
+## Represents the smallest possible difference between two double precision
+## floating point numbers.
+const EPSILON_DOUBLE_PRECISION: float = 2.0 ** -52.0
+
+## Represents the smallest possible difference between two single precision
+## floating point numbers.
+const EPSILON_SINGLE_PRECISION: float = 2.0 ** -22.0
