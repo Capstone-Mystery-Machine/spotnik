3031601a4b10d0636b043a70c320c95ce697b88b
oridim <125223080+oridim@users.noreply.github.com>
2026-01-13 10:24:18 -0500

    docs: added docstrings for input controllers + input mode system (#14)
    * docs: added docstrings for `InputX` singleton
    
    * docs: initial non-impl docstrings for `GyroInputController`
    
    * docs: initial non-impl docstrings for `MouseInputController`
    
    * docs: initial non-impl docstrings for `TouchInputController`
    
    * docs: added impl docstrings for the input controller scripts

--- scenes/mouse_input_controller.gd
+++ scenes/mouse_input_controller.gd
@@ -1,17 +1,35 @@
 extends Node
+## Input controller that targets a [Node3D].
+##
+## The input controller modifies a target [Node3D]'s [member Node3D.rotation]
+## in response to the end-user click-and-dragging on the 3D viewport.
 
 @export_group("Targeting")
 
+## Represents which [Node3D] that the input controller is going to modify in
+## reaction to input performed by the end-user.
 @export var target_node_3d: Node3D = null
 
 @export_group("Control Settings")
 
+## Represents the sensitivity value that is [b]multiplied against[/b] the
+## end-user's vertical mouse movements. That is, the lower the value, the more
+## physical mouse movement needed to modify the target [Node3D]'s
+## [member Node3D.rotation].
 @export var sensitity_pitch: float = 0.025
 
+## Represents the sensitivity value that is [b]multiplied against[/b] the
+## end-user's horizontal mouse movements. That is, the lower the value, the more
+## physical mouse movement needed to modify the target [Node3D]'s
+## [member Node3D.rotation].
 @export var sensitity_yaw: float = 0.025
 
+## Represents if the user is currently holding down the interaction action while
+## moving their mouse cursor across the 3D viewport.
 var is_dragging: bool = false
 
+## Enables the input controller if [constant InputX.input_mode] is set to
+## [constant InputX.InputMode.INPUT_MOUSE].
 func _ready() -> void:
 	match InputX.input_mode:
 		InputX.InputMode.INPUT_MOUSE:
@@ -19,6 +37,12 @@ func _ready() -> void:
 		_:
 			process_mode = Node.PROCESS_MODE_DISABLED
 
+## Handles unhandled mouse input by translating screen pixel movement deltas
+## into angular rotation applied to the target [Node3D]'s
+## [member Node3D.rotation].
+## [br]
+## Only runs while the end-user is holding down the buttons assigned to the
+## [code]interaction_drag[/code] action.
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		is_dragging = event.is_action_pressed("interaction_drag")
