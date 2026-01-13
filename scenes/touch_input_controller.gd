d8df0f6accdce5d1932de2d139486027d7a70d63
oridim <125223080+oridim@users.noreply.github.com>
2026-01-13 15:39:42 -0500

    fix: added pitch max / min exports to mouse / touch input controllers to prevent gimbal lock

--- scenes/touch_input_controller.gd
+++ scenes/touch_input_controller.gd
@@ -12,6 +12,18 @@ extends Node
 
 @export_group("Control Settings")
 
+## Represents how far up the end-user can rotate the target [Node3D]'s
+## [member Node3D.rotation] exclusively. This is mainly so that the end-user
+## cannot rotate the [Node3D] up so much that it rotates back on itself, thus
+## causing gimbal lock.
+@export_range(-90.0, 90.0, 0.0001) var pitch_max: float = 90.0
+
+## Represents how far down the end-user can rotate the target [Node3D]'s
+## [member Node3D.rotation] exclusively. This is mainly so that the end-user
+## cannot rotate the [Node3D] down so much that it rotates back on itself, thus
+## causing gimbal lock.
+@export_range(-90.0, 90.0, 0.0001) var pitch_min: float = -90.0
+
 ## Represents the sensitivity value that is [b]multiplied against[/b] the
 ## end-user's vertical touch movements. That is, the lower the value, the more
 ## physical dragging movement needed to modify the target [Node3D]'s
@@ -23,6 +35,16 @@ extends Node
 ## physical dragging movement needed to modify the target [Node3D]'s
 ## [member Node3D.rotation].
 @export var sensitivity_yaw: float = 0.0025
+
+## Represents the max pitch inspector values converted from degrees into radians.
+var _pitch_max: float:
+	get:
+		return deg_to_rad(pitch_max - GlobalScopeX.EPSILON_SINGLE_PRECISION)
+
+## Represents the min pitch inspector values converted from degrees into radians.
+var _pitch_min: float:
+	get:
+		return deg_to_rad(pitch_min + GlobalScopeX.EPSILON_SINGLE_PRECISION)
 
 
 ## Enables the input controller if [constant InputX.input_mode] is set to
@@ -45,7 +67,12 @@ func _unhandled_input(event: InputEvent) -> void:
 			event.screen_relative.x * sensitivity_yaw,
 		)
 
-		target_node_3d.rotation.x -= rotation_delta.x
+		target_node_3d.rotation.x = clamp(
+			target_node_3d.rotation.x - rotation_delta.x,
+			_pitch_min,
+			_pitch_max,
+		)
+
 		target_node_3d.rotation.y -= rotation_delta.y
 
 		get_viewport().set_input_as_handled()
