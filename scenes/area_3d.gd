a4c153b26a14f6e23c363014bb34d7a82e56d713
1Dannyj1 <235363862+1Dannyj1@users.noreply.github.com>
2026-02-09 17:52:02 -0500

    chore: adhere to formatter

--- scenes/area_3d.gd
+++ scenes/area_3d.gd
@@ -3,7 +3,7 @@ extends Area3D
 @export var inner_radius: float = 0.5
 
 # Bodies currently inside the INNER radius
-var inner_bodies: Dictionary[CollisionObject3D, bool] = {}
+var inner_bodies: Dictionary[CollisionObject3D, bool] = { }
 
 signal inner_entered(body: CollisionObject3D)
 signal inner_exited(body: CollisionObject3D)
@@ -13,11 +13,13 @@ signal outer_exited(body: CollisionObject3D)
 var space_state: PhysicsDirectSpaceState3D
 var inner_shape: SphereShape3D
 
+
 func _ready() -> void:
 	space_state = get_world_3d().direct_space_state
 
 	inner_shape = SphereShape3D.new()
 	inner_shape.radius = inner_radius
+
 
 func _process(_delta: float) -> void:
 	var query := PhysicsShapeQueryParameters3D.new()
@@ -28,7 +30,7 @@ func _process(_delta: float) -> void:
 
 	var results: Array = space_state.intersect_shape(query)
 
-	var currently_inside: Dictionary[CollisionObject3D, bool] = {}
+	var currently_inside: Dictionary[CollisionObject3D, bool] = { }
 
 	for hit in results:
 		var body := hit.collider as CollisionObject3D
@@ -49,11 +51,12 @@ func _process(_delta: float) -> void:
 			emit_signal("inner_exited", body)
 			inner_bodies.erase(body)
 
+
 func _on_body_entered(body: CollisionObject3D) -> void:
 	print(body.name, " entered OUTER area")
 	emit_signal("outer_entered", body)
 
+
 func _on_body_exited(body: CollisionObject3D) -> void:
-	print(body.name, " exited OUTER area")
 	emit_signal("outer_exited", body)
 	inner_bodies.erase(body)
