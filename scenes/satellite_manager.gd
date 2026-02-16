70ea25f00087500613922af2e14e10f45a881c0f
1Dannyj1 <235363862+1Dannyj1@users.noreply.github.com>
2026-02-16 17:43:15 -0500

    even more formatting fixes

--- scenes/satellite_manager.gd
+++ scenes/satellite_manager.gd
@@ -5,47 +5,51 @@ extends Node3D
 
 var satellite_data: Array = []
 
+
 func _ready() -> void:
 	load_satellite_data()
 	spawn_all_landmarks()
+
 
 func load_satellite_data() -> void:
 	if not FileAccess.file_exists(json_path):
 		push_error("JSON file not found at: " + json_path)
 		return
-	
+
 	var file = FileAccess.open(json_path, FileAccess.READ)
 	var json_text = file.get_as_text()
 	file.close()
-	
+
 	var parsed = JSON.parse_string(json_text)
-	
+
 	if typeof(parsed) != TYPE_ARRAY:
 		push_error("JSON format invalid!")
 		return
-	
+
 	satellite_data = parsed
+
 
 func spawn_all_landmarks() -> void:
 	for data in satellite_data:
 		spawn_landmark(data)
 
+
 func spawn_landmark(data: Dictionary) -> void:
 	if landmark_scene == null:
 		return
-	
+
 	var landmark = landmark_scene.instantiate()
 	add_child(landmark)
-	
+
 	# Convert JSON position array → Vector3
 	var pos_array = data["position"]
 	var satPosition = Vector3(pos_array[0], pos_array[1], pos_array[2])
 	landmark.position = satPosition
-	
+
 	landmark.setup(
 		data["int_designator"],
 		data["norad_id"],
 		data["name"],
 		data["country"],
-		data["launch"]
+		data["launch"],
 	)
