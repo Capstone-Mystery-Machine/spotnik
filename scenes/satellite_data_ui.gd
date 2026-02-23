bf619e4412df9646a9f6970f417a9a1fb703be4b
1Dannyj1 <235363862+1Dannyj1@users.noreply.github.com>
2026-02-23 12:40:33 -0500

    linting error fixes

--- scenes/satellite_data_ui.gd
+++ scenes/satellite_data_ui.gd
@@ -4,16 +4,16 @@ extends Node3D
 
 @onready var viewport = $Info_Sprite
 @onready var viewport_container = $Info_Sprite/Info_Viewport_Container
-@onready var International_Designator = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/International_Designator2
-@onready var Norad_Catalog_Identifier = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Norad_Catalog_Identifier2
-@onready var Name = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Name2
-@onready var Origin_Country = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Origin_Country2
-@onready var Launch_Date = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Launch_Date2
-@onready var Latitude = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Latitude2
-@onready var Longitude = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Longitude2
-@onready var Cartesian_x = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Cartesian_x2
-@onready var Cartesian_y = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Cartesian_y2
-@onready var Cartesian_z = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Cartesian_z2
+@onready var international_designator = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/International_Designator2
+@onready var norad_catalog_identifier = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Norad_Catalog_Identifier2
+@onready var satellite_name = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Name2
+@onready var origin_country = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Origin_Country2
+@onready var launch_date = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Launch_Date2
+@onready var latitude = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Latitude2
+@onready var longitude = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Longitude2
+@onready var cartesian_x = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Cartesian_x2
+@onready var cartesian_y = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Cartesian_y2
+@onready var cartesian_z = $Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Cartesian_z2
 
 
 func _ready():
@@ -24,16 +24,16 @@ func _ready():
 func _ui_info_signal(landmark):
 	viewport_container.visible = true
 
-	International_Designator.text = landmark.international_designator
-	Norad_Catalog_Identifier.text = landmark.norad_catalog_id
-	Name.text = landmark.satellite_name
-	Origin_Country.text = landmark.country
-	Launch_Date.text = str(landmark.launch_date)
-	Latitude.text = str(landmark.latitude)
-	Longitude.text = str(landmark.longitude)
-	Cartesian_x.text = "N/A"
-	Cartesian_y.text = 'N/A'
-	Cartesian_z.text = 'N/A'
+	international_designator.text = landmark.international_designator
+	norad_catalog_identifier.text = landmark.norad_catalog_id
+	satellite_name.text = landmark.satellite_name
+	origin_country.text = landmark.country
+	launch_date.text = str(landmark.launch_date)
+	latitude.text = str(landmark.latitude)
+	longitude.text = str(landmark.longitude)
+	cartesian_x.text = "N/A"
+	cartesian_y.text = 'N/A'
+	cartesian_z.text = 'N/A'
 
 
 func _on_close_button_pressed() -> void:
