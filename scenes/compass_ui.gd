2cf8cc0ce1af5f1ee28a96650485687f4bee32b4
Stewart <169107728+MintyStew@users.noreply.github.com>
2026-02-24 14:42:24 -0500

    Update compass_ui.gd

--- scenes/compass_ui.gd
+++ scenes/compass_ui.gd
@@ -9,17 +9,7 @@ extends Node3D
 
 func _ready() -> void:
 	var gyro_data = load_gyro_data()
-
-	var n = compass.instantiate()
-	add_child(n)
-	var e = compass.instantiate()
-	add_child(e)
-	var s = compass.instantiate()
-	add_child(s)
-	var w = compass.instantiate()
-	add_child(w)
-
-	spawn_compass(gyro_data, n, e, s, w)
+	align_compass(gyro_data)
 
 
 func load_gyro_data() -> Dictionary:
@@ -32,18 +22,18 @@ func load_gyro_data() -> Dictionary:
 	return cardinals
 
 
-func spawn_compass(gyro_data: Dictionary, n, e, s, w) -> void:
+func align_compass(gyro_data: Dictionary) -> void:
 	var spawn_dist = 1000
 	gyro_data["North"] = Vector3(gyro_data["North"].x * spawn_dist, gyro_data["North"].y * spawn_dist, gyro_data["North"].z)
 	gyro_data["East"] = Vector3(gyro_data["East"].x * spawn_dist, gyro_data["East"].y * spawn_dist, gyro_data["East"].z)
 	gyro_data["South"] = Vector3(gyro_data["South"].x * spawn_dist, gyro_data["South"].y * spawn_dist, gyro_data["South"].z)
 	gyro_data["West"] = Vector3(gyro_data["West"].x * spawn_dist, gyro_data["West"].y * spawn_dist, gyro_data["West"].z)
 	#
-	#north_label_sprite.location = gyro_data["North"]
-	#east_label_sprite.location = gyro_data["East"]
-	#south_label_sprite.location = gyro_data["South"]
-	#west_label_sprite.location = gyro_data["West"]
-	n.position = gyro_data["North"]
-	e.position = gyro_data["East"]
-	s.position = gyro_data["South"]
-	w.position = gyro_data["West"]
+	north_label_sprite.position = gyro_data["North"]
+	east_label_sprite.position = gyro_data["East"]
+	south_label_sprite.position = gyro_data["South"]
+	west_label_sprite.position = gyro_data["West"]
+	#n.position = gyro_data["North"]
+	#e.position = gyro_data["East"]
+	#s.position = gyro_data["South"]
+	#w.position = gyro_data["West"]
