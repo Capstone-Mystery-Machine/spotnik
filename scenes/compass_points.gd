4990c2cd5dca4ecad1d12841e5e9286d95410f76
Stewart <169107728+MintyStew@users.noreply.github.com>
2026-02-24 14:11:24 -0500

    cardinal directions represented in 3D environment,
    cardinal directions represented in 3D environment
    not aligned to true directions when in gryoscopic mode

--- scenes/compass_points.gd
+++ scenes/compass_points.gd
@@ -1,10 +1,14 @@
 extends Node3D
 
+@onready var north_label_sprite = $SpriteNorth
+@onready var east_label_sprite = $SpriteEast
+@onready var south_label_sprite = $SpriteSouth
+@onready var west_label_sprite = $SpriteWest
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	north_label_sprite.position = Vector3(20, -2, 0)
+	east_label_sprite.position = Vector3(0, -2, 20)
+	south_label_sprite.position = Vector3(-20, -2, 0)
+	west_label_sprite.position = Vector3(0, -2, -20)
