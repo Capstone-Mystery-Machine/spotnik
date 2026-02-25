3258676a791756de94b8508cc27c2699740d7740
oridim <125223080+oridim@users.noreply.github.com>
2026-02-25 01:58:27 -0500

    refactor: move projection and simulation settings to the top of skybox settings

--- resources/skybox_settings.gd
+++ resources/skybox_settings.gd
@@ -5,6 +5,122 @@ extends Resource
 
 signal property_changed(property_name: StringName)
 
+@export_group("Projection Settings")
+
+## Represents the projection distance of the skybox meshes.
+@export_range(0.0, 1000.0, 0.00001, "suffix:m") var projection_radius: float = 1.0:
+	set(value):
+		if projection_radius != value:
+			projection_radius = value
+			emit_changed()
+			property_changed.emit(&"projection_radius")
+
+## Represents the radius multiplier for the mid and far nebulae layers.
+@export_range(1.0, 2.0, 0.00001) var nebulae_displacement_multiplier: float = 1.1:
+	set(value):
+		if nebulae_displacement_multiplier != value:
+			nebulae_displacement_multiplier = value
+			emit_changed()
+			property_changed.emit(&"nebulae_displacement_multiplier")
+
+## Represents the radius multiplier for the point stars layers.
+@export_range(1.0, 2.0, 0.00001) var stars_point_radius_multiplier: float = 1.0125:
+	set(value):
+		if stars_point_radius_multiplier != value:
+			stars_point_radius_multiplier = value
+			emit_changed()
+			property_changed.emit(&"stars_point_radius_multiplier")
+
+## Represents the radius multiplier for subsequent point stars layers.
+@export_range(1.0, 2.0, 0.00001) var stars_point_displacement_multiplier: float = 1.25:
+	set(value):
+		if stars_point_displacement_multiplier != value:
+			stars_point_displacement_multiplier = value
+			emit_changed()
+			property_changed.emit(&"stars_point_displacement_multiplier")
+
+## Represents the radius multiplier for the field stars layers.
+@export_range(1.0, 2.0, 0.00001) var stars_field_radius_multiplier: float = 1.0125:
+	set(value):
+		if stars_field_radius_multiplier != value:
+			stars_field_radius_multiplier = value
+			emit_changed()
+			property_changed.emit(&"stars_field_radius_multiplier")
+
+## Represents the radius multiplier for subsequent field stars layers.
+@export_range(1.0, 2.0, 0.00001) var stars_field_displacement_multiplier: float = 1.025:
+	set(value):
+		if stars_field_displacement_multiplier != value:
+			stars_field_displacement_multiplier = value
+			emit_changed()
+			property_changed.emit(&"stars_field_displacement_multiplier")
+
+## Represents the radius multiplier for the void layer.
+@export_range(1.0, 2.0, 0.00001) var void_radius_multiplier: float = 1.0125:
+	set(value):
+		if void_radius_multiplier != value:
+			void_radius_multiplier = value
+			emit_changed()
+			property_changed.emit(&"void_radius_multiplier")
+
+@export_group("Simulation Settings")
+
+## Represents the base rotation speed for the void layer.
+@export_range(-0.1, 0.1, 0.00001, "suffix:rad/s") var rotation_speed: float = 0.00025:
+	set(value):
+		if rotation_speed != value:
+			rotation_speed = value
+			emit_changed()
+			property_changed.emit(&"rotation_speed")
+
+## Represents the rotation speed multiplier for the far field stars.
+@export_range(-2.0, 2.0, 0.00001) var stars_field_far_speed_multiplier: float = 1.0:
+	set(value):
+		if stars_field_far_speed_multiplier != value:
+			stars_field_far_speed_multiplier = value
+			emit_changed()
+			property_changed.emit(&"stars_field_far_speed_multiplier")
+
+## Represents the rotation speed multiplier for the near field stars.
+@export_range(-2.0, 2.0, 0.00001) var stars_field_near_speed_multiplier: float = 1.0:
+	set(value):
+		if stars_field_near_speed_multiplier != value:
+			stars_field_near_speed_multiplier = value
+			emit_changed()
+			property_changed.emit(&"stars_field_near_speed_multiplier")
+
+## Represents the rotation speed multiplier for the far point stars.
+@export_range(-2.0, 2.0, 0.00001) var stars_point_far_speed_multiplier: float = 1.0:
+	set(value):
+		if stars_point_far_speed_multiplier != value:
+			stars_point_far_speed_multiplier = value
+			emit_changed()
+			property_changed.emit(&"stars_point_far_speed_multiplier")
+
+## Represents the rotation speed multiplier for the mid point stars.
+@export_range(-2.0, 2.0, 0.00001) var stars_point_mid_speed_multiplier: float = 1.005:
+	set(value):
+		if stars_point_mid_speed_multiplier != value:
+			stars_point_mid_speed_multiplier = value
+			emit_changed()
+			property_changed.emit(&"stars_point_mid_speed_multiplier")
+
+## Represents the rotation speed multiplier for the near point stars.
+@export_range(-2.0, 2.0, 0.00001) var stars_point_near_speed_multiplier: float = 1.01:
+	set(value):
+		if stars_point_near_speed_multiplier != value:
+			stars_point_near_speed_multiplier = value
+			emit_changed()
+			property_changed.emit(&"stars_point_near_speed_multiplier")
+
+## Represents the rotation speed multiplier for the nebulae.
+@export_range(-2.0, 2.0, 0.00001) var nebulae_speed_multiplier: float = 0.99:
+	set(value):
+		if nebulae_speed_multiplier != value:
+			nebulae_speed_multiplier = value
+			emit_changed()
+			property_changed.emit(&"nebulae_speed_multiplier")
+
 @export_group("Materials Settings")
 
 @export_subgroup("Nebulae Settings")
@@ -130,122 +246,6 @@ signal property_changed(property_name: StringName)
 			stars_field_far_material = value
 			emit_changed()
 			property_changed.emit(&"stars_field_far_material")
-
-@export_group("Projection Settings")
-
-## Represents the projection distance of the skybox meshes.
-@export_range(0.0, 1000.0, 0.00001, "suffix:m") var projection_radius: float = 1.0:
-	set(value):
-		if projection_radius != value:
-			projection_radius = value
-			emit_changed()
-			property_changed.emit(&"projection_radius")
-
-## Represents the radius multiplier for the mid and far nebulae layers.
-@export_range(1.0, 2.0, 0.00001) var nebulae_displacement_multiplier: float = 1.1:
-	set(value):
-		if nebulae_displacement_multiplier != value:
-			nebulae_displacement_multiplier = value
-			emit_changed()
-			property_changed.emit(&"nebulae_displacement_multiplier")
-
-## Represents the radius multiplier for the point stars layers.
-@export_range(1.0, 2.0, 0.00001) var stars_point_radius_multiplier: float = 1.0125:
-	set(value):
-		if stars_point_radius_multiplier != value:
-			stars_point_radius_multiplier = value
-			emit_changed()
-			property_changed.emit(&"stars_point_radius_multiplier")
-
-## Represents the radius multiplier for subsequent point stars layers.
-@export_range(1.0, 2.0, 0.00001) var stars_point_displacement_multiplier: float = 1.25:
-	set(value):
-		if stars_point_displacement_multiplier != value:
-			stars_point_displacement_multiplier = value
-			emit_changed()
-			property_changed.emit(&"stars_point_displacement_multiplier")
-
-## Represents the radius multiplier for the field stars layers.
-@export_range(1.0, 2.0, 0.00001) var stars_field_radius_multiplier: float = 1.0125:
-	set(value):
-		if stars_field_radius_multiplier != value:
-			stars_field_radius_multiplier = value
-			emit_changed()
-			property_changed.emit(&"stars_field_radius_multiplier")
-
-## Represents the radius multiplier for subsequent field stars layers.
-@export_range(1.0, 2.0, 0.00001) var stars_field_displacement_multiplier: float = 1.025:
-	set(value):
-		if stars_field_displacement_multiplier != value:
-			stars_field_displacement_multiplier = value
-			emit_changed()
-			property_changed.emit(&"stars_field_displacement_multiplier")
-
-## Represents the radius multiplier for the void layer.
-@export_range(1.0, 2.0, 0.00001) var void_radius_multiplier: float = 1.0125:
-	set(value):
-		if void_radius_multiplier != value:
-			void_radius_multiplier = value
-			emit_changed()
-			property_changed.emit(&"void_radius_multiplier")
-
-@export_group("Simulation Settings")
-
-## Represents the base rotation speed for the void layer.
-@export_range(-0.1, 0.1, 0.00001, "suffix:rad/s") var rotation_speed: float = 0.00025:
-	set(value):
-		if rotation_speed != value:
-			rotation_speed = value
-			emit_changed()
-			property_changed.emit(&"rotation_speed")
-
-## Represents the rotation speed multiplier for the far field stars.
-@export_range(-2.0, 2.0, 0.00001) var stars_field_far_speed_multiplier: float = 1.0:
-	set(value):
-		if stars_field_far_speed_multiplier != value:
-			stars_field_far_speed_multiplier = value
-			emit_changed()
-			property_changed.emit(&"stars_field_far_speed_multiplier")
-
-## Represents the rotation speed multiplier for the near field stars.
-@export_range(-2.0, 2.0, 0.00001) var stars_field_near_speed_multiplier: float = 1.0:
-	set(value):
-		if stars_field_near_speed_multiplier != value:
-			stars_field_near_speed_multiplier = value
-			emit_changed()
-			property_changed.emit(&"stars_field_near_speed_multiplier")
-
-## Represents the rotation speed multiplier for the far point stars.
-@export_range(-2.0, 2.0, 0.00001) var stars_point_far_speed_multiplier: float = 1.0:
-	set(value):
-		if stars_point_far_speed_multiplier != value:
-			stars_point_far_speed_multiplier = value
-			emit_changed()
-			property_changed.emit(&"stars_point_far_speed_multiplier")
-
-## Represents the rotation speed multiplier for the mid point stars.
-@export_range(-2.0, 2.0, 0.00001) var stars_point_mid_speed_multiplier: float = 1.005:
-	set(value):
-		if stars_point_mid_speed_multiplier != value:
-			stars_point_mid_speed_multiplier = value
-			emit_changed()
-			property_changed.emit(&"stars_point_mid_speed_multiplier")
-
-## Represents the rotation speed multiplier for the near point stars.
-@export_range(-2.0, 2.0, 0.00001) var stars_point_near_speed_multiplier: float = 1.01:
-	set(value):
-		if stars_point_near_speed_multiplier != value:
-			stars_point_near_speed_multiplier = value
-			emit_changed()
-			property_changed.emit(&"stars_point_near_speed_multiplier")
-
-## Represents the rotation speed multiplier for the nebulae.
-@export_range(-2.0, 2.0, 0.00001) var nebulae_speed_multiplier: float = 0.99:
-	set(value):
-		if nebulae_speed_multiplier != value:
-			nebulae_speed_multiplier = value
-			emit_changed()
-			property_changed.emit(&"nebulae_speed_multiplier")
 
 @export_group("Nebulae Effects Settings")
 
