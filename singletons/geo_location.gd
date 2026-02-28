aeff7c13115e729751460616b3b0483087c5bc33
oridim <125223080+oridim@users.noreply.github.com>
2026-02-28 18:15:51 -0500

    refactor: switch to more "provider" terminology since some platforms might not have native location APIs

--- singletons/geo_location.gd
+++ singletons/geo_location.gd
@@ -1,38 +1,64 @@
 class_name GeoLocation
 extends RefCounted
+## A singleton for handling Spotnik-specific geo-location logic.
+##
+## The [b]GeoLocation[/b] singleton handles the loading and data abstraction
+## of platform-specific geo-location providers.
 
 signal location_changed(location_data: LocationData)
 
+## Represents the [GeoLocation] singleton.
 static var instance: GeoLocation = GeoLocation.new()
 
-var _plugin_instance: Variant = null
+## Represents the runtime instance of the loaded geo-location provider.
+var _provider_instance: Variant = null
 
+## Represents the most recently retrieved geographical location data.
 var location_data: LocationData = null
 
-const PluginName = {
+## Represents the available geo-location provider engine names.
+## [br]
+## [br]
+## Each provider name:
+## [br]
+## • Corresponds to a specific platform-level implementation or Godot plugin.
+const ProviderName = {
+	## Spotnik is configured to not utilize a geo-location provider.
+	## [br]
+	## [br]
+	## [b]Behavior:[/b] Geo-location initialization is skipped.
 	NONE = null,
-	ANDROID_PLUGIN = &"PraxisMapperGPSPlugin",
+
+	## Spotnik is configured to use the Android GPS plugin.
+	## [br]
+	## [br]
+	## [b]Behavior:[/b] Connects to the [code]PraxisMapperGPSPlugin[/code] plugin.
+	ANDROID_PROVIDER = &"PraxisMapperGPSPlugin",
 }
 
 
-func _get_geo_location_plugin_name() -> StringName:
-	return ProjectSettings.get_setting("spotnik/geo_location/plugin_name")
+## Returns the geo-location provider name specified in the project settings.
+func _get_geo_location_provider_name() -> StringName:
+	return ProjectSettings.get_setting("spotnik/geo_location/provider_name")
 
 
+## Initializes the Android-specific geo-location provider and connects its signals.
 func _on_android_init() -> void:
-	_plugin_instance = Engine.get_singleton(PluginName.ANDROID_PLUGIN)
+	_provider_instance = Engine.get_singleton(ProviderName.ANDROID_PROVIDER)
 
-	if _plugin_instance == null:
+	if _provider_instance == null:
 		push_error(
-			"bad dispatch to 'GeoLocation._on_android_init' (failed to load"
-			+ "plugin '" + PluginName.ANDROID_PLUGIN + "')",
+			"bad dispatch to 'GeoLocation._on_android_init' (failed to load plugin '%s')"
+			% ProviderName.ANDROID_PROVIDER,
 		)
 
 		Engine.get_main_loop().quit()
 
-	_plugin_instance.onLocationUpdates.connect(_on_android_location_changed)
+	_provider_instance.onLocationUpdates.connect(_on_android_location_changed)
 
 
+## Translates the Android geo-location provider's location data into a [LocationData]
+## object and emits it.
 func _on_android_location_changed(location: Dictionary[String, float]) -> void:
 	location_data = LocationData.new(
 		location.latitude,
@@ -42,27 +68,41 @@ func _on_android_location_changed(location: Dictionary[String, float]) -> void:
 	emit_signal("location_changed", location_data)
 
 
-func init_plugin() -> void:
-	if _plugin_instance != null:
-		push_error("bad dispatch to 'GeoLocation.init_plugin' (plugin was already loaded)")
+## Initializes the geo-location provider based on the engine export's specific project
+## settings.
+func init_provider() -> void:
+	if _provider_instance != null:
+		push_error(
+			"bad dispatch to 'GeoLocation.init_provider' (provider was already loaded)",
+		)
+
 		return
 
-	var plugin_name = _get_geo_location_plugin_name()
+	var provider_name = _get_geo_location_provider_name()
 
-	print("'GeoLocation.init_plugin': trying to load geo-location plugin '%s'" % plugin_name)
+	print(
+		"'GeoLocation.init_provider': trying to load geo-location provider '%s'"
+		% provider_name,
+	)
 
-	match plugin_name:
-		PluginName.ANDROID_PLUGIN:
+	match provider_name:
+		ProviderName.ANDROID_PROVIDER:
 			_on_android_init()
 		null:
-			print("'GeoLocation.init_plugin': no geo-location plugin was specified, skipping")
+			print(
+				"'GeoLocation.init_provider': no geo-location provider was specified, skipping",
+			)
+
 			return
 		_:
 			push_error(
-				"bad dispatch to 'GeoLocation.init_plugin' (plugin '"
-				+ plugin_name + "' not supported)",
+				"bad dispatch to 'GeoLocation.init_provider' (provider '%s' not supported)"
+				% provider_name,
 			)
 
 			Engine.get_main_loop().quit()
 
-	print("'GeoLocation.init_plugin': geo-location plugin '%s' successfully loaded" % plugin_name)
+	print(
+		"'GeoLocation.init_provider': geo-location provider '%s' successfully loaded"
+		% provider_name,
+	)
