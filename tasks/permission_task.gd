7946d09afcfb1caaccc1af2af9b95c4230067534
oridim <125223080+oridim@users.noreply.github.com>
2026-03-12 15:45:31 -0400

    refactor: changed `PermissionTask` to evaluate permission grants once all permissions have been granted or denied
    This allows us to handle tricky Android permissions like location permissions.
    
    On Android, if fine granted location access is granted, then both fine and coarse are granted. If coarse access is granted, then fine is denied and coarse is granted. If no location access is granted, then both coarse and fine is denied.
    
    To Spotnik, it doesn't matter too much if the user chooses coarse over fine. But, the behaivour of permission grants makes coding handling a bit trickier. So, it's easier to just defer handling til every permission has been resp;ved.
    
    Also, support for checking when _all_ permissions have been added with this refactor. So that this task works with any other permissions we may want down the line.

--- tasks/permission_task.gd
+++ tasks/permission_task.gd
@@ -1,6 +1,10 @@
 extends Node
-## Requests the permissions required by the app's export target when the node
-## is made ready.
+## Requests the permissions required by the app's export target when the task
+## is requested to run.
+## [br]
+## [b]NOTE:[/b] This task should only be ran once per-boot of Spotnik.
+
+signal permissions_granted
 
 # gdlint-ignore-next-line constant-name
 const PermissionName = {
@@ -8,9 +12,40 @@ const PermissionName = {
 	ANDROID_FINE_LOCATION = &"android.permission.ACCESS_FINE_LOCATION",
 }
 
-var _was_geo_location_initialized: bool = false
+# gdlint-ignore-next-line constant-name
+const PermissionKey = {
+	PermissionName.ANDROID_COARSE_LOCATION: true,
+	PermissionName.ANDROID_FINE_LOCATION: true,
+}
+
+var _expected_responses: int = 0
 
 @onready var scene_tree: SceneTree = get_tree()
+
+
+func _dialog_permission_declined(permission: String) -> void:
+	DisplayServer.dialog_show(
+		"Permission Not Granted",
+		"Spotnik was not granted a required permission: " + permission + ". The app will exit now.",
+		PackedStringArray(["OK"]),
+		func(_button_index: int):
+			print("'PermissionTask._dialog_declined': exiting due to declined " + permission)
+			scene_tree.quit(OSX.ExitCode.PERMISSION_NOT_GRANTED),
+	)
+
+
+func _on_all_permissions_resolved() -> void:
+	var granted := OS.get_granted_permissions()
+
+	var has_location_permission = PermissionName.ANDROID_COARSE_LOCATION in granted \
+	or PermissionName.ANDROID_FINE_LOCATION in granted
+
+	if not has_location_permission:
+		_dialog_permission_declined("location")
+		return
+
+	_on_all_permissions_granted()
+	permissions_granted.emit()
 
 
 func _on_all_permissions_granted() -> void:
@@ -18,64 +53,43 @@ func _on_all_permissions_granted() -> void:
 
 
 func _on_geo_location_permission_granted() -> void:
-	print(
-		"'PermissionManager._on_geo_location_permission_granted': geo-location permission granted",
-	)
+	print("'PermissionTask._on_geo_location_permission_granted': geo-location permission granted")
 
 	GeoLocation.instance.init_provider()
-	_was_geo_location_initialized = true
 
 
-func _on_permission_declined(permission: String) -> void:
-	match permission:
-		PermissionName.ANDROID_COARSE_LOCATION:
-			if PermissionName.ANDROID_FINE_LOCATION in OS.get_granted_permissions():
-				return
-		PermissionName.ANDROID_FINE_LOCATION:
-			if PermissionName.ANDROID_COARSE_LOCATION in OS.get_granted_permissions():
-				return
+func _on_request_permissions_result(permission: String, _granted: bool) -> void:
+	if permission in PermissionKey:
+		_expected_responses -= 1
 
-	if scene_tree.on_request_permissions_result.is_connected(_on_request_permissions_result):
-		scene_tree.on_request_permissions_result.disconnect(_on_request_permissions_result)
+	if _expected_responses <= 0:
+		if scene_tree.on_request_permissions_result.is_connected(_on_request_permissions_result):
+			scene_tree.on_request_permissions_result.disconnect(_on_request_permissions_result)
 
-	DisplayServer.dialog_show(
-		"Permission Not Granted",
-		"Spotnik was not granted a required permission. The app will exit now.",
-		PackedStringArray(["OK"]),
-		func(_button_index: int):
-			print(
-				"'PermissionManager._on_permission_declined': permission '" \
-				+ permission + "' was not granted, exiting",
-			)
-
-			scene_tree.quit(OSX.ExitCode.PERMISSION_NOT_GRANTED),
-	)
+		_on_all_permissions_resolved()
 
 
-func _on_permission_granted(permission: String) -> void:
-	match permission:
-		PermissionName.ANDROID_COARSE_LOCATION, PermissionName.ANDROID_FINE_LOCATION:
-			if not _was_geo_location_initialized:
-				_on_geo_location_permission_granted()
+func _task() -> void:
+	print("'PermissionTask._task': trying to request permissions")
 
+	if not OSX.permission_request_required():
+		print("'PermissionTask._task': permissions requests not needed, skipping requests")
 
-func _on_request_permissions_result(permission: String, granted: bool) -> void:
-	if granted:
-		_on_permission_granted(permission)
+		_on_all_permissions_granted()
+		return
 
-	else:
-		_on_permission_declined(permission)
+	var granted_permissions := OS.get_granted_permissions()
 
+	for permission_key in PermissionKey:
+		if permission_key not in granted_permissions:
+			_expected_responses += 1
 
-func _ready() -> void:
-	print("'PermissionManager._ready': trying to request permissions")
-
-	if not OSX.permission_request_required() or OS.request_permissions():
-		print(
-			"'PermissionManager._ready': permissions were already granted or not needed, skipping",
-		)
+	if _expected_responses == 0 or OS.request_permissions():
+		print("'PermissionTask._task': permissions were already granted, skipping requests")
 
 		_on_all_permissions_granted()
 		return
 
 	scene_tree.on_request_permissions_result.connect(_on_request_permissions_result)
+
+	await permissions_granted
