7996215ff5164df6d647048ad6fad23a42b27660
oridim <125223080+oridim@users.noreply.github.com>
2026-03-13 02:26:09 -0400

    feat: poll GeoIP service for geo-location data as a fallthrough on non-specialized platforms (#145)
    * feat: configured Spotnik to use GeoIP service as fall through for Geo-Location
    
    * refactor: made separate duration interval for GeoIP querying
    
    * refactor: these aren't nodes, they are more basic ref-counted objects
    
    * feat: added `GlobalScopeX.fetch`, `GlobalScopeX.fetch_utf8_string`, and `GlobalScopeX.fetch_json` helper functions for working with non-`Node` based HTTP requests
    
    * feat: added implementation of GeoIP provider
    
    * fix: consuming headers before response check
    
    Apparently if you do that, then the response check will return `false`. Amazing.
    
    * fix: not using right member name of JSON response
    
    * refactor: use direct reference to signal instead of `emit_signal`
    
    * fix: do not reset location data on bad responses, we can just lazily keep the older values til acceptable response
    
    * feat: add error message for null provider responses
    
    * refactor: GeoLocation singleton API to be completely static except for the instance for the signal
    
    * refactor: moved geo-location provider init into specialized task
    
    * feat: added `GeoLocationTask` to bootstrapping scenes
    
    * feat: updated `GeoLocationTask` to await for the first geo-location payload before continuing

--- singletons/geo_location.gd
+++ singletons/geo_location.gd
@@ -7,24 +7,34 @@ extends RefCounted
 
 signal location_changed(location_data: LocationData)
 
-## Represents the minimum distance in meters for the geo-location of the end-user
-## to be queried again.
-## [br]
-## [b]NOTE:[/b] Not every geo-location provider supports this feature.
-const UPDATE_DISTANCE_INTERVAL = 2.0
+const GEOIP_HOST: String = "ip-api.com"
+
+const GEOIP_PATH: String = "/json/?fields=status,message,lat,lon"
+
+const GEOIP_PORT: int = 80
 
 ## Represents the minimum duration in milliseconds for the geo-location of the
-## end-user to be queried again.
-const UPDATE_DURATION_INTERVAL = 3000
+## end-user to be queried again when via GeoIP HTTP service.
+const GEOIP_UPDATE_DURATION_INTERVAL: float = 1000 * 60 * 5
+
+## Represents the minimum distance in meters for the geo-location of the end-user
+## to be queried again when via sensors.
+## [br]
+## [b]NOTE:[/b] Not every geo-location provider supports this feature.
+const SENSOR_UPDATE_DISTANCE_INTERVAL: float = 2.0
+
+## Represents the minimum duration in milliseconds for the geo-location of the
+## end-user to be queried again when via sensors.
+const SENSOR_UPDATE_DURATION_INTERVAL: float = 1000 * 3
 
 ## Represents the [GeoLocation] singleton.
 static var instance: GeoLocation = GeoLocation.new()
 
 ## Represents the runtime instance of the loaded geo-location provider.
-var _provider_instance: Variant = null
+static var _provider_instance: Variant = null
 
 ## Represents the most recently retrieved geographical location data.
-var location_data: LocationData = null
+static var location_data: LocationData = null
 
 ## Represents the available geo-location provider engine names.
 ## [br]
@@ -45,11 +55,17 @@ const ProviderName = {
 	## [br]
 	## [b]Behavior:[/b] Connects to the [code]PraxisMapperGPSPlugin[/code] plugin.
 	ANDROID_PROVIDER = &"PraxisMapperGPSPlugin",
+
+	## Spotnik is configured to use the GeoIP HTTP service.
+	## [br]
+	## [br]
+	## [b]Behavior:[/b] Connects to the [code]ip-api.com[/code] HTTP service.
+	GEOIP_PROVIDER = &"ip-api.com",
 }
 
 
 ## Initializes the Android-specific geo-location provider and connects its signals.
-func _on_android_init() -> void:
+static func _on_android_init() -> void:
 	_provider_instance = Engine.get_singleton(ProviderName.ANDROID_PROVIDER) as JNISingleton
 
 	if _provider_instance == null:
@@ -63,26 +79,69 @@ func _on_android_init() -> void:
 
 	_provider_instance.onLocationUpdates.connect(_on_android_location_changed)
 
-	_provider_instance.SetMinDistMeters(UPDATE_DISTANCE_INTERVAL)
-	_provider_instance.SetMinTimeMs(UPDATE_DURATION_INTERVAL)
+	_provider_instance.SetMinDistMeters(SENSOR_UPDATE_DISTANCE_INTERVAL)
+	_provider_instance.SetMinTimeMs(SENSOR_UPDATE_DURATION_INTERVAL)
 
 	_provider_instance.StartListening()
 
 
 ## Translates the Android geo-location provider's location data into a [LocationData]
 ## object and emits it.
-func _on_android_location_changed(location: Dictionary) -> void:
+static func _on_android_location_changed(location: Dictionary) -> void:
 	location_data = LocationData.new(
 		location.latitude,
 		location.longitude,
 	)
 
-	emit_signal("location_changed", location_data)
+	instance.location_changed.emit(location_data)
+
+
+## Initializes the GeoIP geo-location provider and connects its signals.
+static func _on_geoip_init() -> void:
+	var scene_tree = Engine.get_main_loop()
+
+	while true:
+		await _on_geoip_poll()
+		await scene_tree.create_timer(GEOIP_UPDATE_DURATION_INTERVAL / 1000).timeout
+
+
+## Polls GeoIP HTTP service and then translates the provider's location data into
+## a [LocationData] object and emits it.
+static func _on_geoip_poll() -> void:
+	var response = await GlobalScopeX.fetch_json(
+		false,
+		GEOIP_HOST,
+		GEOIP_PORT,
+		GEOIP_PATH,
+	)
+
+	if response == null:
+		push_error(
+			"bad dispatch to '_on_geoip_poll' (no response from provider)",
+		)
+
+		return
+
+	var body = response.body
+
+	if body.status != "success":
+		push_error(
+			"bad dispatch to '_on_geoip_poll' (provider returned error %s)" % body.message,
+		)
+
+		return
+
+	location_data = LocationData.new(
+		body.lat,
+		body.lon,
+	)
+
+	instance.location_changed.emit(location_data)
 
 
 ## Initializes the geo-location provider based on the engine export's specific project
 ## settings.
-func init_provider() -> void:
+static func init_provider() -> void:
 	if _provider_instance != null:
 		push_error(
 			"bad dispatch to 'GeoLocation.init_provider' (provider was already loaded)",
@@ -102,6 +161,8 @@ func init_provider() -> void:
 	match provider_name:
 		ProviderName.ANDROID_PROVIDER:
 			_on_android_init()
+		ProviderName.GEOIP_PROVIDER:
+			_on_geoip_init()
 		null:
 			print(
 				"'GeoLocation.init_provider': no geo-location provider was specified, skipping",
