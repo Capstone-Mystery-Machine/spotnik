9391d53913f16988b7d817337613c6ad0b36a6d1
oridim <125223080+oridim@users.noreply.github.com>
2026-03-13 00:35:48 -0400

    feat: added `GlobalScopeX.fetch`, `GlobalScopeX.fetch_utf8_string`, and `GlobalScopeX.fetch_json` helper functions for working with non-`Node` based HTTP requests

--- singletons/global_scope_x.gd
+++ singletons/global_scope_x.gd
@@ -9,3 +9,140 @@ const EPSILON_DOUBLE_PRECISION: float = 2.0 ** -52.0
 ## Represents the smallest possible difference between two single precision
 ## floating point numbers.
 const EPSILON_SINGLE_PRECISION: float = 2.0 ** -22.0
+
+
+static func fetch(
+		use_tls: bool,
+		host: String,
+		port: int,
+		path: String,
+		method: int = HTTPClient.METHOD_GET,
+		headers: Dictionary[String, String] = { },
+) -> Variant:
+	var client = HTTPClient.new()
+	var scene_tree = Engine.get_main_loop()
+	var tls_options = TLSOptions.client() if use_tls else null
+
+	var error = client.connect_to_host(host, port, tls_options)
+
+	if error != OK:
+		push_error(
+			"bad arguments #0, #1, or #2 to 'GlobalScopeX.fetch' (failed to establish" \
+			+ " a connection to '" + host + ":" + str(port) + "')",
+		)
+
+		return null
+
+	while client.get_status() == HTTPClient.STATUS_CONNECTING \
+	or client.get_status() == HTTPClient.STATUS_RESOLVING:
+		client.poll()
+		await scene_tree.process_frame
+
+	if client.get_status() != HTTPClient.STATUS_CONNECTED:
+		push_error(
+			"bad arguments #0, #1, or #2 to 'GlobalScopeX.fetch' (failed to resolve" \
+			+ " a connection to '" + host + ":" + str(port) + "')",
+		)
+
+		return null
+
+	var packed_headers = PackedStringArray()
+
+	for key in headers:
+		packed_headers.append("%s: %s" % [key, headers[key]])
+
+	error = client.request(method, path, packed_headers)
+
+	if error != OK:
+		push_error(
+			"bad arguments #3, #4, or #5 to 'GlobalScopeX.fetch' (request could not be sent)",
+		)
+
+		return null
+
+	while client.get_status() == HTTPClient.STATUS_REQUESTING:
+		client.poll()
+		await scene_tree.process_frame
+
+	var response_headers = client.get_response_headers_as_dictionary()
+
+	if !client.has_response():
+		return {
+			"headers": response_headers,
+			"body": null,
+		}
+
+	var response_body = PackedByteArray()
+
+	while client.get_status() == HTTPClient.STATUS_BODY:
+		client.poll()
+
+		var chunk = client.read_response_body_chunk()
+
+		if chunk.size() <= 0:
+			await scene_tree.process_frame
+			continue
+
+		response_body.append_array(chunk)
+
+	return {
+		"headers": response_headers,
+		"body": response_body,
+	}
+
+
+static func fetch_json(
+		use_tls: bool,
+		host: String,
+		port: int,
+		path: String,
+		method: int = HTTPClient.METHOD_GET,
+		headers: Dictionary[String, String] = { },
+) -> Variant:
+	var json = JSON.new()
+	var json_headers = headers.duplicate()
+
+	json_headers.Accept = "application/json"
+
+	var response = await fetch_utf8_string(
+		use_tls,
+		host,
+		port,
+		path,
+		method,
+		json_headers,
+	)
+
+	if response == null:
+		return null
+
+	var body = response.body as String
+
+	response.body = json.get_data() if json.parse(body) == OK else null
+	return response
+
+
+static func fetch_utf8_string(
+		use_tls: bool,
+		host: String,
+		port: int,
+		path: String,
+		method: int = HTTPClient.METHOD_GET,
+		headers: Dictionary[String, String] = { },
+) -> Variant:
+	var response = await fetch(use_tls, host, port, path, method, headers)
+
+	if response == null:
+		return null
+
+	var body = response.body as PackedByteArray
+
+	if body == null:
+		push_error(
+			"bad dispatch to 'GlobalScopeX.fetch_utf8_string' (response body was 'null')",
+		)
+
+		return null
+
+	response.body = body.get_string_from_utf8()
+	return response
