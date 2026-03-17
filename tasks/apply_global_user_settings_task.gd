ee518cebba3a629f5b5ad027e325461af685d96c
oridim <125223080+oridim@users.noreply.github.com>
2026-03-16 22:13:58 -0400

    feat: added logging to new tasks

--- tasks/apply_global_user_settings_task.gd
+++ tasks/apply_global_user_settings_task.gd
@@ -2,4 +2,8 @@ extends TaskNode
 ## Applies all non-specific global user settings to the game engine.
 
 func _task() -> void:
+	print(
+		"'ApplyGlobalUserSettingsTask._task': applying globally applicable user settings",
+	)
+
 	UserSettings.apply_global_settings()
--- tasks/load_user_settings_task.gd
+++ tasks/load_user_settings_task.gd
@@ -2,7 +2,19 @@ extends TaskNode
 ## Loads end-user's currently stored settings, if available.
 
 func _task() -> void:
+	print(
+		"'LoadUserSettingsTask._task': trying to load user settings on disk",
+	)
+
 	if not UserSettings.has_file():
+		print(
+			"'LoadUserSettingsTask._task': user settings not found on disk, skipping",
+		)
+
 		return
+
+	print(
+		"'LoadUserSettingsTask._task': user settings found on disk, loading",
+	)
 
 	UserSettings.load()
