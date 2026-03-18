a2b03c14ebdbf19f7a8dc5282505971da0ecfde4
oridim <125223080+oridim@users.noreply.github.com>
2026-03-18 16:33:02 -0400

    refactor: moved user settings disk load and global application to `UserSettings._init`
    I do **not** like this since then it requires the side effect of something trying to access `UserSettings.instance` for the code to even run. _But_, this does allow non-bootstraped scenes to get the disk data when ran directly from the editor.

--- singletons/user_settings.gd
+++ singletons/user_settings.gd
@@ -302,3 +302,27 @@ static func load() -> Error:
 
 static func save() -> Error:
 	return _settings.save(PATH_USER_SETTINGS_FILE)
+
+
+func _init() -> void:
+	print(
+		"'UserSettings._init': trying to load user settings on disk",
+	)
+
+	if UserSettings.has_file():
+		print(
+			"'UserSettings._init': user settings found on disk, loading",
+		)
+
+		UserSettings.load()
+
+	else:
+		print(
+			"'UserSettings._init': user settings not found on disk, skipping",
+		)
+
+	print(
+		"'UserSettings._init': applying global engine-level user settings",
+	)
+
+	UserSettings.apply_global_settings()
--- tasks/apply_global_user_settings_task.gd
+++
@@ -1,9 +0,0 @@
-extends TaskNode
-## Applies all non-specific global user settings to the game engine.
-
-func _task() -> void:
-	print(
-		"'ApplyGlobalUserSettingsTask._task': applying globally applicable user settings",
-	)
-
-	UserSettings.apply_global_settings()
