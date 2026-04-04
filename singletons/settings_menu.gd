03f2f9d083a44464ff238b26fc139cf14fd749a6
oridim <125223080+oridim@users.noreply.github.com>
2026-04-04 19:26:33 -0400

    refactor: simplified `SettingsMenu` singleton API

--- singletons/settings_menu.gd
+++ singletons/settings_menu.gd
@@ -4,28 +4,21 @@ static var _settings_menu_screen: SettingsMenuScreen = null
 
 static var visible: bool:
 	get():
-		if not is_instance_valid(_settings_menu_screen):
-			_connect_settings_menu_screen()
-
+		_get_settings_menu_screen()
 		return _settings_menu_screen.visible
 
 
-static func _connect_settings_menu_screen() -> void:
-	var scene_tree = Engine.get_main_loop() as SceneTree
-	_settings_menu_screen = scene_tree.get_first_node_in_group("SettingsMenuScreen")
-
-	# TODO: bind signals
+static func _get_settings_menu_screen() -> void:
+	if not is_instance_valid(_settings_menu_screen):
+		var scene_tree = Engine.get_main_loop() as SceneTree
+		_settings_menu_screen = scene_tree.get_first_node_in_group("SettingsMenuScreen")
 
 
 static func close() -> void:
-	if not is_instance_valid(_settings_menu_screen):
-		_connect_settings_menu_screen()
-
+	_get_settings_menu_screen()
 	_settings_menu_screen.visible = false
 
 
 static func open() -> void:
-	if not is_instance_valid(_settings_menu_screen):
-		_connect_settings_menu_screen()
-
+	_get_settings_menu_screen()
 	_settings_menu_screen.visible = true
