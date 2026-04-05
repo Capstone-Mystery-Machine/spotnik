cc09dc989f0673988d4732303ebcd2813fa7e3af
Stewart <169107728+MintyStew@users.noreply.github.com>
2026-04-04 21:48:03 -0400

    reworking info ui toward interactable 3d object

--- nodes/satellite_data_ui.gd
+++ nodes/satellite_data_ui.gd
@@ -1,38 +1,29 @@
 extends Node3D
 
-@onready var viewport = $Info_Sprite
-@onready var viewport_container = $Info_Sprite/Info_Viewport_Container
-@onready var international_designator = \
-$Info_Sprite/Info_Viewport_Container/Info_Viewport\
-/PanelContainer/Satel_Info/International_Designator2
-@onready var norad_catalog_identifier = \
-$Info_Sprite/Info_Viewport_Container/Info_Viewport\
-/PanelContainer/Satel_Info/Norad_Catalog_Identifier2
-@onready var satellite_name = \
-$Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Name2
-@onready var origin_country = \
-$Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Origin_Country2
-@onready var launch_date = \
-$Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Launch_Date2
-@onready var latitude = \
-$Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Latitude2
-@onready var longitude = \
-$Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Longitude2
-@onready var cartesian_x = \
-$Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Cartesian_x2
-@onready var cartesian_y = \
-$Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Cartesian_y2
-@onready var cartesian_z = \
-$Info_Sprite/Info_Viewport_Container/Info_Viewport/PanelContainer/Satel_Info/Cartesian_z2
+@onready var info_ui = %Info_UI
+@onready var international_designator = %International_Designator2
+@onready var norad_catalog_identifier = %Norad_Catalog_Identifier2
+@onready var satellite_name = %Name2
+@onready var origin_country = %Origin_Country2
+@onready var launch_date = %Launch_Date2
+@onready var latitude = %Latitude2
+@onready var longitude = %Longitude2
+@onready var cartesian_x = %Cartesian_x2
+@onready var cartesian_y = %Cartesian_y2
+@onready var cartesian_z = %Close_Button
 
 
 func _ready():
-	viewport_container.visible = false
+	info_ui.visible = false
 	SignalBus.instance.ui_info.connect(_ui_info_signal)
+
+	#node_area.mouse_entered.connect(_mouse_entered_area)
+	#node_area.mouse_exited.connect(_mouse_exited_area)
+	#node_area.input_event.connect(_mouse_input_event)
 
 
 func _ui_info_signal(landmark):
-	viewport_container.visible = true
+	info_ui.visible = true
 
 	international_designator.text = landmark.international_designator
 	norad_catalog_identifier.text = landmark.norad_catalog_id
@@ -47,8 +38,101 @@ func _ui_info_signal(landmark):
 
 
 func _on_close_button_pressed() -> void:
-	viewport_container.visible = false
+	info_ui.visible = false
+
+#func _on_landmark_ui_open() -> void:
+#	info_ui.visible = true
+
+# Used for checking if the mouse is inside the Area3D.
+var is_mouse_inside = false
+# The last processed input touch/mouse event. To calculate relative movement.
+var last_event_pos2D = null
+# The time of the last event in seconds since engine start.
+var last_event_time: float = -1.0
+
+@onready var node_viewport = %Info_Viewport
+@onready var node_quad = %Quad
+@onready var node_area = %Area3D
 
 
-func _on_landmark_ui_open() -> void:
-	viewport_container.visible = true
+func _mouse_entered_area():
+	is_mouse_inside = true
+
+
+func _mouse_exited_area():
+	is_mouse_inside = false
+
+
+func _unhandled_input(event):
+	# Check if the event is a non-mouse/non-touch event
+	for mouse_event in [InputEventMouseButton, InputEventMouseMotion, InputEventScreenDrag, InputEventScreenTouch]:
+		if is_instance_of(event, mouse_event):
+			# If the event is a mouse/touch event, then we can ignore it here, because it will be
+			# handled via Physics Picking.
+			return
+	node_viewport.push_input(event)
+
+
+func _mouse_input_event(_camera: Camera3D, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int):
+	# Get mesh size to detect edges and make conversions. This code only support PlaneMesh and QuadMesh.
+	var quad_mesh_size = node_quad.mesh.size
+
+	# Event position in Area3D in world coordinate space.
+	var event_pos3D = event_position
+
+	# Current time in seconds since engine start.
+	var now: float = Time.get_ticks_msec() / 1000.0
+
+	# Convert position to a coordinate space relative to the Area3D node.
+	# NOTE: affine_inverse accounts for the Area3D node's scale, rotation, and position in the scene!
+	event_pos3D = node_quad.global_transform.affine_inverse() * event_pos3D
+
+	# TODO: Adapt to bilboard mode or avoid completely.
+
+	var event_pos2D: Vector2 = Vector2()
+
+	if is_mouse_inside:
+		# Convert the relative event position from 3D to 2D.
+		event_pos2D = Vector2(event_pos3D.x, -event_pos3D.y)
+
+		# Right now the event position's range is the following: (-quad_size/2) -> (quad_size/2)
+		# We need to convert it into the following range: -0.5 -> 0.5
+		event_pos2D.x = event_pos2D.x / quad_mesh_size.x
+		event_pos2D.y = event_pos2D.y / quad_mesh_size.y
+		# Then we need to convert it into the following range: 0 -> 1
+		event_pos2D.x += 0.5
+		event_pos2D.y += 0.5
+
+		# Finally, we convert the position to the following range: 0 -> viewport.size
+		event_pos2D.x *= node_viewport.size.x
+		event_pos2D.y *= node_viewport.size.y
+		# We need to do these conversions so the event's position is in the viewport's coordinate system.
+
+	elif last_event_pos2D != null:
+		# Fall back to the last known event position.
+		event_pos2D = last_event_pos2D
+
+	# Set the event's position and global position.
+	event.position = event_pos2D
+	if event is InputEventMouse:
+		event.global_position = event_pos2D
+
+	# Calculate the relative event distance.
+	if event is InputEventMouseMotion or event is InputEventScreenDrag:
+		# If there is not a stored previous position, then we'll assume there is no relative motion.
+		if last_event_pos2D == null:
+			event.relative = Vector2(0, 0)
+		# If there is a stored previous position, then we'll calculate the relative position by subtracting
+		# the previous position from the new position. This will give us the distance the event traveled from prev_pos.
+		else:
+			event.relative = event_pos2D - last_event_pos2D
+			event.velocity = event.relative / (now - last_event_time)
+
+	# Update last_event_pos2D with the position we just calculated.
+	last_event_pos2D = event_pos2D
+
+	# Update last_event_time to current time.
+	last_event_time = now
+
+	# Finally, send the processed input event to the viewport.
+	node_viewport.push_input(event)
