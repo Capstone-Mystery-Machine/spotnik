07e9a29388c2f198c4799915ecd20804ccb9c807
oridim <125223080+oridim@users.noreply.github.com>
2026-04-08 17:17:21 -0400

    fix: `DualBorderTravelers` now clips itself to the corner radius
    This is so that that inner `ContentPanelContainer` can clip its own children

--- shaders/dual_border_travelers.gd
+++ shaders/dual_border_travelers.gd
@@ -21,5 +21,26 @@ func _update_rotation() -> void:
 		)
 
 
+func _update_corners() -> void:
+	if is_node_ready() and _shader_material:
+		RenderingServer.material_set_param(
+			_shader_material.get_rid(),
+			"corner_radius",
+			Vector4(32.0, 32.0, 32.0, 32.0),
+		)
+
+
+func _update_size() -> void:
+	if is_node_ready() and _shader_material:
+		RenderingServer.material_set_param(
+			_shader_material.get_rid(),
+			"size",
+			size,
+		)
+
+
 func _ready() -> void:
+	resized.connect(_update_size)
+
+	_update_corners()
 	_update_rotation()
