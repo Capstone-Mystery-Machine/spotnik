fbdd4cbc54676d94860aaad19d1b84917fe3f04d
oridim <125223080+oridim@users.noreply.github.com>
2026-04-18 01:35:18 -0400

    feat: added `RootScene.transition_to` to orchestrate transitions to new scenes

--- scenes/root_scene.gd
+++ scenes/root_scene.gd
@@ -1,10 +1,36 @@
 class_name RootScene
 extends Node
 
-@onready var taskManager: TaskManager = %TaskManager
+enum TransitionType {
+	DEFAULT,
+}
+
+@onready var content_container: Node = %ContentContainer
+@onready var task_manager: TaskManager = %TaskManager
 
 static var instance: RootScene:
 	get:
 		var scene_tree = Engine.get_main_loop() as SceneTree
 
 		return scene_tree.current_scene
+
+
+func transition_to(
+		target_scene: PackedScene,
+		transition_type: TransitionType = TransitionType.DEFAULT,
+		tasks: Array[TaskNode] = [],
+) -> void:
+	for task in tasks:
+		task_manager.add_child(task)
+
+	print("animate in")
+
+	await task_manager.run_all_tasks()
+
+	for child in content_container.get_children():
+		child.queue_free()
+
+	if target_scene:
+		content_container.add_child(target_scene.instantiate())
+
+	print("animate out")
