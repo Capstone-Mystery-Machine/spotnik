12737b8e367a0854c7fb5887e7e490097b032fe3
oridim <125223080+oridim@users.noreply.github.com>
2026-04-18 01:11:52 -0400

    refactor: shifted task execution functionality of `TaskManager` into `TaskManager.run_all_tasks` to be available to other scripts

--- tasks/task_manager.gd
+++ tasks/task_manager.gd
@@ -1,12 +1,12 @@
 extends Node
 
-func _ready() -> void:
+func run_all_tasks() -> void:
 	var task_nodes = get_children() as Array[TaskNode]
 
-	print("'TaskManager._ready': running available tasks")
+	print("'TaskManager.run_all_tasks': running available tasks")
 
 	for task_node in task_nodes:
-		print("'TaskManager._ready': found task '%s', running" % task_node.name)
+		print("'TaskManager.run_all_tasks': found task '%s', running" % task_node.name)
 
 		# HACK: Some tasks may be async and some tasks might not be. So, we can
 		# just ignore this warning. Sadly, we cannot explicitly type `TaskNode._task`
@@ -18,6 +18,10 @@ func _ready() -> void:
 		# gdlint-ignore-next-line private-access
 		await task_node._task()
 
-		print("'TaskManager._ready': task '%s' ran successfully" % task_node.name)
+		print("'TaskManager.run_all_tasks': task '%s' ran successfully" % task_node.name)
 
-	print("'TaskManager._ready': all available tasks complete")
+	print("'TaskManager.run_all_tasks': all available tasks complete")
+
+
+func _ready() -> void:
+	run_all_tasks()
