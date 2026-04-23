396e9cdb075c1e5c5137c882e066ea2b4ddd12ed
oridim <125223080+oridim@users.noreply.github.com>
2026-04-23 16:55:01 -0400

    refactor: move satellite details UI into separate scene

--- ui/screens/satellite_details_screen.gd
+++ ui/screens/satellite_details_screen.gd
@@ -1,5 +1,5 @@
-class_name SatelUI
-extends Node3D
+class_name SatelliteDetailsScreen
+extends PanelContainer
 
 @onready var info_ui = %Info_UI
 @onready var international_designator = %International_Designator2
@@ -7,22 +7,12 @@ extends Node3D
 @onready var satellite_name = %Name2
 @onready var origin_country = %Origin_Country2
 @onready var launch_date = %Launch_Date2
-@onready var panel_container: PanelContainer = %PanelContainer
 
 # var to determine dist of UI in proportion to satel dist
 @onready var info_ui_dist = 0.025
 
-var content_alpha: float = 1.0:
-	set(value):
-		content_alpha = value
 
-		if not is_node_ready():
-			return
-
-		panel_container.modulate.a = value
-
-
-func setup_satel_ui(landmark: Landmark) -> void:
+func update_satellite_details(landmark: Landmark) -> void:
 	international_designator.text = landmark.international_designator
 	norad_catalog_identifier.text = landmark.norad_catalog_id
 	satellite_name.text = landmark.satellite_name
