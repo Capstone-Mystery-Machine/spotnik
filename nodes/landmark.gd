d815f105def82f9323d15788362237c3c0c132f7
1Dannyj1 <235363862+1Dannyj1@users.noreply.github.com>
2026-04-23 16:35:31 -0400

    landmark adjustment (#175)
    * landmark adjustment
    
    * different fix

--- nodes/landmark.gd
+++ nodes/landmark.gd
@@ -63,12 +63,17 @@ func _ready() -> void:
 
 	screen_notifier.screen_entered.connect(_on_screen_entered)
 	screen_notifier.screen_exited.connect(_on_screen_exited)
+
 	_set_active(screen_notifier.is_on_screen())
 
 
 func _physics_process(delta: float) -> void:
 	update_orbit_motion(delta)
-	update_visual_scale(delta)
+
+	if outer_shape.disabled:
+		visual_scale = lerp(visual_scale, 1.0, min(scale_speed * delta, 1.0))
+	else:
+		update_visual_scale(delta)
 
 
 func update_orbit_motion(delta: float) -> void:
@@ -115,13 +120,10 @@ func compute_gravity() -> Vector3:
 
 
 func _set_active(active: bool) -> void:
-	set_physics_process(active)
-	detector.monitoring = active
-	detector.monitorable = active
+	outer_shape.set_deferred("disabled", not active)
 
 	if not active:
 		pointer = null
-		visual_scale = 1.0
 
 
 func _on_screen_entered() -> void:
