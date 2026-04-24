e8db16e0ca25c9baa67a47f011b1f928d4048861
oridim <125223080+oridim@users.noreply.github.com>
2026-04-23 22:15:05 -0400

    feat: satellite details UI revamp (#177)
    * refactor: move satellite details UI into separate scene
    
    * fix: formatting
    
    * refactor: converted satellite details UI to use stylings of settings menu
    
    * feat: added two more smaller `DisplayLabelN` theme tiers
    
    * refactor: scaled down satellite details UI headers
    
    * feat: scaling tiers for plain labels
    
    * refactor: rescaled satellite details body text
    
    * feat: added smaller content panel margins theme variations
    
    * refactor: updated satellite info UI to use smaller margins
    
    * feat: added `ControlProjecter` for projecting `Control` nodes into 3D spaces
    
    * remove: old code
    
    * remove: unused code
    
    * fix: launch date text
    
    * remove: unused code
    
    * refactor: satellite details UI to use `ControlProjector`

---
+++ ui/screens/satellite_details_screen.gd
@@ -0,0 +1,19 @@
+class_name SatelliteDetailsScreen
+extends MarginContainer
+
+@onready var control_projector: ControlProjector = %ControlProjector
+@onready var international_designator_label = %InternationalDesignatorLabel
+@onready var launch_date_label = %LaunchDateLabel
+@onready var norad_catalog_identifier_label = %NoradCatalogIdentifierLabel
+@onready var origin_country_label = %OriginCountryLabel
+@onready var satellite_name_label = %SatelliteNameLabel
+
+
+func update_satellite_details(landmark: Landmark) -> void:
+	international_designator_label.text = landmark.international_designator
+	norad_catalog_identifier_label.text = landmark.norad_catalog_id
+	satellite_name_label.text = landmark.satellite_name
+	origin_country_label.text = landmark.country
+	launch_date_label.text = landmark.launch_date
+
+	control_projector.target_node = landmark
