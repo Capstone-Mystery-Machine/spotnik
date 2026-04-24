189318b3e33ee793181dabafbba28c7ae4dd46fa
oridim <125223080+oridim@users.noreply.github.com>
2026-04-23 21:14:11 -0400

    refactor: converted satellite details UI to use stylings of settings menu

--- ui/screens/satellite_details_screen.gd
+++ ui/screens/satellite_details_screen.gd
@@ -1,23 +1,16 @@
 class_name SatelliteDetailsScreen
-extends PanelContainer
+extends MarginContainer
 
-@onready var info_ui = %Info_UI
-@onready var international_designator = %International_Designator2
-@onready var norad_catalog_identifier = %Norad_Catalog_Identifier2
-@onready var satellite_name = %Name2
-@onready var origin_country = %Origin_Country2
+@onready var international_designator_label = %InternationalDesignatorLabel
+@onready var norad_catalog_identifier_label = %NoradCatalogIdentifierLabel
+@onready var satellite_name_label = %SatelliteNameLabel
+@onready var origin_country_label = %OriginCountryLabel
 @onready var launch_date = %Launch_Date2
-
-# var to determine dist of UI in proportion to satel dist
-@onready var info_ui_dist = 0.025
 
 
 func update_satellite_details(landmark: Landmark) -> void:
-	international_designator.text = landmark.international_designator
-	norad_catalog_identifier.text = landmark.norad_catalog_id
-	satellite_name.text = landmark.satellite_name
-	origin_country.text = landmark.country
+	international_designator_label.text = landmark.international_designator
+	norad_catalog_identifier_label.text = landmark.norad_catalog_id
+	satellite_name_label.text = landmark.satellite_name
+	origin_country_label.text = landmark.country
 	launch_date.text = landmark.launch_date
-
-	#set spawn position
-	info_ui.position = landmark.position * info_ui_dist
